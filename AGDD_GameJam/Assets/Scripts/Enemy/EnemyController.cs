6403f46c05031520a05e6282e0b092f20aa5a458
Kristinn Björgvinsson <kristinnb17@ru.is>
2020-03-12 21:45:26 +0000

    Enemy on hit, player attack and a basic map implemented

--- AGDD_GameJam/Assets/Scripts/Enemy/EnemyController.cs
+++ AGDD_GameJam/Assets/Scripts/Enemy/EnemyController.cs
@@ -4,21 +4,31 @@ namespace Enemy
 {
     public class EnemyController : MonoBehaviour, IEnemy
     {
+        // Animator to set animation variables
+        private Animator _animator;
+        // Variable to set the death animation
+        private static readonly int IsDead = Animator.StringToHash("IsDead");
+
         // Start is called before the first frame update
         void Start()
         {
         
+            _animator = gameObject.GetComponent<Animator>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            Debug.Log("VAR");
         }
 
+        /// <summary>
+        /// Called when player hits enemy
+        /// </summary>
+        /// <param name="damage">amount of damage enemy takes</param>
         public void Hit(int damage)
         {
-            throw new System.NotImplementedException();
+            // Set player as dead
+            _animator.SetBool("IsDead", true);
         }
 
         public void Move()
---
+++ AGDD_GameJam/Assets/Scripts/Player/SwordDetection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Enemy;
+using UnityEngine;
+
+public class SwordDetection : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        IEnemy enemy = other.gameObject.GetComponentInParent<IEnemy>();
+        Debug.Log(enemy);
+        enemy?.Hit(1);
+    }
+}
