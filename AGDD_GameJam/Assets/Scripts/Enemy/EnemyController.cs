65a0d648cfa5032ef91a55d6c21f1f969abaeb66
Kristinn Björgvinsson <kristinnb17@ru.is>
2020-03-12 16:18:24 +0000

    Enemy

---
+++ AGDD_GameJam/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Enemy
+{
+    public class EnemyController : MonoBehaviour, IEnemy
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log("VAR");
+        }
+
+        public void Hit(int damage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Move()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsDetected(PlayerController player)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AGDD_GameJam/Assets/Scripts/Enemy/IEnemy.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Enemy
+{
+    /// <summary>
+    /// Interface of enemy
+    /// </summary>
+    public interface IEnemy
+    {
+        /// <summary>
+        /// Called when player hits enemy
+        /// </summary>
+        /// <param name="damage">amount of damage enemy takes</param>
+        void Hit(int damage);
+        /// <summary>
+        /// Called to move the enemy around the map
+        /// </summary>
+        void Move(); // Might take direction parameter
+
+        /// <summary>
+        /// Checks if enemy detects player
+        /// </summary>
+        /// <param name="player">Instance of the player</param>
+        /// <returns>True if enemy is detected, false if not</returns>
+        bool IsDetected(PlayerController player);
+    }
+}
