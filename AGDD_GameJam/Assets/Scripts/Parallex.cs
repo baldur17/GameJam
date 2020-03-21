89eab3a2831b00f8ae16de138f53408089c963c9
unknown <baldur17@ru.is>
2020-03-21 16:45:46 +0000

    simple normal map added to tiles and parallex background implemented, might need styling/resizing or change sprites

---
+++ AGDD_GameJam/Assets/Scripts/Parallex.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallex : MonoBehaviour
+{
+    public float parallaxEffect;
+    private Vector3 _lastCameraPosition;
+    private float _textureUnitSizeX;
+    private Transform _cameraTransform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Camera.main != null) _cameraTransform = Camera.main.transform;
+        _lastCameraPosition = _cameraTransform.position;
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        _textureUnitSizeX = texture.width / sprite.pixelsPerUnit;
+        
+    }
+    
+    void LateUpdate()
+    {
+        Vector3 deltaMovement = _cameraTransform.position - _lastCameraPosition;
+        
+        transform.position += new Vector3(deltaMovement.x * parallaxEffect, deltaMovement.y, 0f);
+
+        _lastCameraPosition = _cameraTransform.position;
+
+        if (Mathf.Abs(_cameraTransform.position.x - transform.position.x) >= _textureUnitSizeX)
+        {
+            float offsetPositionX = (_cameraTransform.position.x - transform.position.x) % _textureUnitSizeX;
+            transform.position = new Vector3(_cameraTransform.position.x + offsetPositionX, transform.position.y, 0.3f);
+        }
+    }
+}
