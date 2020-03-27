ba1938799f810ba7d515006ba3e0822d6daa33cc
Kristinn Björgvinsson <kristinnb17@ru.is>
2020-03-27 18:52:56 +0000

    Pause menu implemented

---
+++ AGDD_GameJam/Assets/PauseMenu.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    [FormerlySerializedAs("GameMenuUi")] public GameObject gameMenuUi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+
+            GameIsPaused = !GameIsPaused;
+        }
+    }
+
+    public void Pause()
+    {
+        gameMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Resume()
+    {
+        gameMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+    
+    
+}
