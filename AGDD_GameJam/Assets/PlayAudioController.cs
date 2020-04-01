0479d1ea86870b3e740e05be9f9cc592dec8a58a
Kristinn Björgvinsson <kristinnb17@ru.is>
2020-04-01 14:44:13 +0000

    Heartbeat audio komid

---
+++ AGDD_GameJam/Assets/PlayAudioController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioController : MonoBehaviour
+{
+
+    public AudioSource clip;
+
+    public void PlayAudio()
+    {
+        clip.Play();
+    }
+    
+}
