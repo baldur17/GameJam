c41a7e7d5c7645f545b9b7ef533019e0fb0b7989
unknown <baldur17@ru.is>
2020-04-02 13:21:30 +0000

    commit before merge

---
+++ AGDD_GameJam/Assets/Scripts/HideAble.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideAble : MonoBehaviour
+{
+    private PlayerController _playerController;
+    void Start()
+    {
+        _playerController = GameManager.instance.player.GetComponent<PlayerController>();
+        Debug.Log("hallo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Player entered hideable object");
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Player left safety");
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("oncollision");
+    }
+}
