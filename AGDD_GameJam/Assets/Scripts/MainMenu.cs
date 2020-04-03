c3e59275da0ee505abd8cbe9009e3c92d81d9566
Kristinn Björgvinsson <kristinnb17@ru.is>
2020-04-03 18:38:17 +0000

    Menu adjusted

--- AGDD_GameJam/Assets/Scripts/MainMenu.cs
+++ AGDD_GameJam/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UIElements;
 
 
 public class MainMenu : MonoBehaviour
@@ -10,6 +11,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     public void StartGame(string sceneName)
     {
+    
         SceneManager.LoadScene(sceneName);
     }
 
