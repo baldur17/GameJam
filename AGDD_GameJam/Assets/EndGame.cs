fb9ff06e959b421da4830cd6e5bebeadee0b8a3f
Kristinn Björgvinsson <kristinnb17@ru.is>
2020-04-06 16:46:44 +0000

    Game Over Screen

---
+++ AGDD_GameJam/Assets/EndGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public PauseMenu menuScript;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        menuScript.FinishGame();
+    }
+}
